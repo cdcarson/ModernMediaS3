48c85805510f9ae02a09da2afb4bea771e34b439
Christopher Carson <chris@modernmedia.co>
2013-12-22 12:02:47 -0600

    added files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Modern Media S3
+Plugin URI: http://modernmedia.co
+Description: Automatically copies media uploads to Amazon S3 for storage and delivery.
+Author: Chris Carson
+Version: 0.1
+Author URI: http://modernmedia.co
+*/
+use ModernMedia\AWSS3\AWSS3Plugin;
+require_once __DIR__ . '/vendor/autoload.php';
+AWSS3Plugin::inst();
